ad2ad509743dc33280b92b6b1494f2dd42bb02b0
MuhamadFahad-coo311349 <fahadimran038@gmail.com>
2026-01-03 23:20:20 +0000

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import React, { Component } from 'react'
 import Navbar from './components/Navbar'
-import News from './components/News'
+import News from './components/News'    
 
 export default class App extends Component {
   render() {
